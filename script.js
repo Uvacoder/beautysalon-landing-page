73f68801fdba38adeef243cd8a6de7770aa1bc72
rafae2k <53544360+rafae2k@users.noreply.github.com>
2021-12-13 15:29:08 -0300

    lint: script

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const toggleMenu = document.querySelectorAll('.toggle-menu');
-const menuOpen = document.querySelector('.menu-open');
 const nav = document.querySelector('header');
 
-toggleMenu.forEach((element) => {
-  element.addEventListener('click', () => {
+toggleMenu.forEach((toggle) => {
+  toggle.addEventListener('click', () => {
     nav.classList.toggle('show');
   });
 });
 
 const navLinks = document.querySelectorAll('.menu a');
 
-navLinks.forEach((element) => {
-  element.addEventListener('click', () => {
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
     nav.classList.remove('show');
   });
 });
