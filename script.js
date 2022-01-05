9387e1ec99531326c540ef8ef52e1b6b9c80f165
rafae2k <53544360+rafae2k@users.noreply.github.com>
2022-01-05 13:13:37 -0300

    style(desktop): resposive desktop @media

--- script.js
+++ script.js
@@ -1,79 +1,113 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-const toggleMenu = document.querySelectorAll('.toggle-menu');
-const nav = document.querySelector('header');
+/* eslint-disable no-restricted-syntax */
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
 
-toggleMenu.forEach((toggle) => {
-  toggle.addEventListener('click', () => {
+for (const element of toggle) {
+  element.addEventListener('click', () => {
     nav.classList.toggle('show');
   });
-});
+}
 
-const navLinks = document.querySelectorAll('.menu a');
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a');
 
-navLinks.forEach((link) => {
+for (const link of links) {
   link.addEventListener('click', () => {
     nav.classList.remove('show');
   });
-});
+}
 
-const headerNav = document.querySelector('.nav');
-const headerHeight = headerNav.offsetHeight;
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY >= headerHeight) {
-    headerNav.classList.add('shadow-scroll');
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll');
   } else {
-    headerNav.classList.remove('shadow-scroll');
+    // menor que a altura do header
+    header.classList.remove('scroll');
   }
-});
+}
 
-// Swiper
-
-const swiper = new Swiper('.swiper', {
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
-  spaceBetween: 5,
-
-  autoplay: {
-    delay: 5000,
-  },
-
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 });
 
-// ScrollReveal
-
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
   origin: 'top',
-  distance: '2rem',
-  duration: 500,
+  distance: '30px',
+  duration: 700,
   reset: true,
 });
 
 scrollReveal.reveal(
-  `
-#home .image, 
-#home .title, 
-#home p, 
-#home .btn-filled,
-#about .image-2,
-#about .title,
-#about p,
-#services .title,
-#services p,
-#services .card,
-#testimonials .title,
-#testimonials .swiper-wrapper,
-#contact .text,
-#contact .contact-info
-`,
-  // eslint-disable-next-line comma-dangle
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
   { interval: 100 }
 );
+
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top');
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]');
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector(`nav ul li a[href*=${sectionId}]`)
+        .classList.add('active');
+    } else {
+      document
+        .querySelector(`nav ul li a[href*=${sectionId}]`)
+        .classList.remove('active');
+    }
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', () => {
+  changeHeaderWhenScroll();
+  backToTop();
+  activateMenuAtCurrentSection();
+});
